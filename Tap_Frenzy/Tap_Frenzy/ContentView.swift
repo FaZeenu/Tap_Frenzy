592e444f191e4d68727c7bfd21856526f9441934
Fathima Zeena <fazeenu123@gmail.com>
2026-07-16 14:55:46 +0530

    Initial Xcode project setup

---
+++ Tap_Frenzy/Tap_Frenzy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tap_Frenzy
+//
+//  Created by zeena on 2026-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tap_Frenzy/Tap_Frenzy/Tap_FrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_FrenzyApp.swift
+//  Tap_Frenzy
+//
+//  Created by zeena on 2026-07-16.
+//
+
+import SwiftUI
+
+@main
+struct Tap_FrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
