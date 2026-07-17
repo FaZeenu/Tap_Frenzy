2f76f8674d988275600b209387267f4e3cf58308
Fathima Zeena <fazeenu123@gmail.com>
2026-07-18 00:40:32 +0530

    Add correct tap scoring

--- Tap_Frenzy/Tap_Frenzy/LightItUpView.swift
+++ Tap_Frenzy/Tap_Frenzy/LightItUpView.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct LightItUpView: View {
     @State private var activeCard = 0
+    @State private var score = 0
+    
     
     let columns = [
         GridItem(.flexible()),
@@ -13,54 +15,57 @@ struct LightItUpView: View {
         ZStack {
             Color.black
                 .ignoresSafeArea()
-
+            
             VStack(spacing: 30) {
-
+                
                 Text("Light It Up")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundStyle(.white)
-
-                Text("Score: 0")
+                
+                Text("Score: \(score)")
                     .font(.title2)
                     .foregroundStyle(.white)
-
+                
                 Text("Time: 30")
                     .font(.title2)
                     .foregroundStyle(.white)
                 
                 LazyVGrid(columns: columns, spacing: 15) {
+                    
                     ForEach(0..<9, id: \.self) { index in
-                        RoundedRectangle(cornerRadius: 15)
-                            .fill(
-                                index == activeCard
-                                ? Color.yellow
-                                : Color.gray
-                            )
                         
-                        
-                            .frame(height: 100)
+                        Button {
+                            handleCardTap(index)
+                        } label: {
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(
+                                    index == activeCard
+                                    ? Color.yellow
+                                    : Color.gray
+                                )
+                                .frame(height: 100)
+                        }
+                        .buttonStyle(.plain)
                     }
                 }
                 .padding()
-                
-                Button("Change Light") {
-                    activeCard = Int.random(in: 0..<9)
-                }
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundStyle(.black)
-                .padding()
-                .background(Color.yellow)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                
+            
                 Spacer()
                 
             }
             .padding(.top, 50)
         }
     }
-}
+        private func handleCardTap(_ index: Int) {
+            if index == activeCard {
+                score += 1
+                activeCard = Int.random(in: 0..<9)
+            }
+        }
+        
+    }
+
 
 #Preview {
     LightItUpView()
